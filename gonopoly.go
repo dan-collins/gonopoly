8b631e054b3c22209800a08ac08d0bc83a872e35
Dan Collins <dcollins@teamworld.com>
2020-06-08 23:31:32 -0400

    Added params for utilities and railroad

--- gonopoly.go
+++ gonopoly.go
@@ -29,14 +29,35 @@ const CardCount = 4
 func Randomize(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 
-	cardCount, cErr := strconv.Atoi(params.Get("cardCount"))
-	playerCount, pErr := strconv.Atoi(params.Get("playerCount"))
-
-	if cErr != nil || pErr != nil || cardCount <= 0 || playerCount <= 0 {
-		http.Error(w, "Bad Card/Player Count", 422)
+	playerCount, err := strconv.Atoi(params.Get("playerCount"))
+	if err != nil || playerCount > 8 || playerCount <= 0 {
+		http.Error(w, "Bad Player Count", 422)
 		return
 	}
-	cardSet, err := run(playerCount, cardCount)
+	utilities, err := strconv.ParseBool(params.Get("utilities"))
+	if err != nil {
+		http.Error(w, "Utilities parameter must be true or false", 422)
+		return
+	}
+	railroads, err := strconv.ParseBool(params.Get("railroads"))
+	if err != nil {
+		http.Error(w, "Railroads parameter must be true or false", 422)
+		return
+	}
+	maxCards := 22
+	if utilities {
+		maxCards += 2
+	}
+	if railroads {
+		maxCards += 4
+	}
+	cardCount, err := strconv.Atoi(params.Get("cardCount"))
+	if err != nil || cardCount <= 0 || cardCount*playerCount > maxCards {
+		http.Error(w, "Bad Card Count", 422)
+		return
+	}
+
+	cardSet, err := getCards(playerCount, cardCount, utilities, railroads)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
@@ -53,7 +74,7 @@ func Randomize(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	cardSet, err := run(4, 4)
+	cardSet, err := getCards(4, 4, true, true)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -66,8 +87,28 @@ func (c *cardSet) AddCard(p property) {
 	c.Total += p.Price
 }
 
-func run(playersNum int, cardCount int) (cards []*cardSet, err error) {
-	props := make([]property, 0, 28)
+func getCards(playersNum int, cardCount int, utilities bool, railroads bool) (cards []*cardSet, err error) {
+	props := createPropertySlice(utilities, railroads)
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
+
+	for i := 1; i <= playersNum; i++ {
+		var cardGroup cardSet
+		cardGroup.Player = fmt.Sprintf("Player %d", i)
+		for c := 0; c < cardCount; c++ {
+			randomIndex := rand.Intn(len(props))
+			cardGroup.AddCard(props[randomIndex])
+			// Remove the element at index i from a.
+			props[randomIndex] = props[len(props)-1] // Copy last element to index i.
+			props[len(props)-1] = property{}         // Erase last element (write zero value).
+			props = props[:len(props)-1]             // Truncate slice.
+		}
+		cards = append(cards, &cardGroup)
+	}
+	return cards, nil
+}
+
+func createPropertySlice(utilities bool, railroads bool) (props []property) {
+
 	props = append(props, property{Name: "Mediterranean Ave.", Price: 60, Color: "#864C38"},
 		property{Name: "Baltic Ave.", Price: 60, Color: "#864C38"},
 		property{Name: "Oriental Ave.", Price: 100, Color: "#AADDF2"},
@@ -89,28 +130,16 @@ func run(playersNum int, cardCount int) (cards []*cardSet, err error) {
 		property{Name: "North Carolina Ave.", Price: 300, Color: "#13A857"},
 		property{Name: "Pennsylvania Ave.", Price: 320, Color: "#13A857"},
 		property{Name: "Park Place", Price: 350, Color: "#0066A4"},
-		property{Name: "Boardwalk", Price: 400, Color: "#0066A4"},
-		property{Name: "Electric Company", Price: 150, Color: "#a5a5a5"},
-		property{Name: "Water Works", Price: 150, Color: "#a5a5a5"},
-		property{Name: "Reading Railroad", Price: 200, Color: "#6b6b6b"},
-		property{Name: "Pennsylvania Railroad", Price: 200, Color: "#6b6b6b"},
-		property{Name: "B. & O. Railroad", Price: 200, Color: "#6b6b6b"},
-		property{Name: "Short Line Railroad", Price: 200, Color: "#6b6b6b"})
-
-	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
-
-	for i := 1; i <= playersNum; i++ {
-		var cardGroup cardSet
-		cardGroup.Player = fmt.Sprintf("Player %d", i)
-		for c := 0; c < cardCount; c++ {
-			randomIndex := rand.Intn(len(props))
-			cardGroup.AddCard(props[randomIndex])
-			// Remove the element at index i from a.
-			props[randomIndex] = props[len(props)-1] // Copy last element to index i.
-			props[len(props)-1] = property{}         // Erase last element (write zero value).
-			props = props[:len(props)-1]             // Truncate slice.
-		}
-		cards = append(cards, &cardGroup)
+		property{Name: "Boardwalk", Price: 400, Color: "#0066A4"})
+	if utilities {
+		props = append(props, property{Name: "Electric Company", Price: 150, Color: "#a5a5a5"},
+			property{Name: "Water Works", Price: 150, Color: "#a5a5a5"})
 	}
-	return cards, nil
+	if railroads {
+		props = append(props, property{Name: "Reading Railroad", Price: 200, Color: "#6b6b6b"},
+			property{Name: "Pennsylvania Railroad", Price: 200, Color: "#6b6b6b"},
+			property{Name: "B. & O. Railroad", Price: 200, Color: "#6b6b6b"},
+			property{Name: "Short Line Railroad", Price: 200, Color: "#6b6b6b"})
+	}
+	return
 }
